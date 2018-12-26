3bcdff2f7bf9574d30882c086077ed709d7d2e06
parvez40 <parvez.mist@gmail.com>
2018-12-26 22:56:13 +0100

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -3,6 +3,5 @@ package oata;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World of ANT!");
-        Systemout.println("Hello World of ANT!");
     }
 }
